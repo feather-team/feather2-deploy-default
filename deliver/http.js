1fdfce382e89b25fd3758c48cbff8b8dd2a72521
zhanghao <jsyczhanghao@sina.com>
2016-09-05 18:18:50 +0800

    fixed bug

--- deliver/http.js
+++ deliver/http.js
@@ -8,16 +8,17 @@ module.exports = function(file, release, content, receiver, next){
             to: release
         }, 
         content, 
-        id,
+        file.id,
         function(err, res){
             if(err || res.trim() != '0'){
                 feather.log.error('upload file [' + file.id + '] to [' + release +
                     '] by receiver [' + receiver + '] error [' + (err || res) + ']');
             }else{
                 var time = '[' + feather.log.now(true) + ']';
-                process.stdout.write(' - '.green.bold + time.grey + ' ' + id + ' >> '.yellow.bold + release + '\n');
+                process.stdout.write(' - '.green.bold + time.grey + ' ' + file.id + ' >> '.yellow.bold + release + '\n');
                 next();
             }
         }
     );
 };
+
